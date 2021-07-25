063b8ddfdb8bb3ca016bc53abc2366ed94844d25
Anthero1 <45021288+Anthero1@users.noreply.github.com>
2021-07-25 13:54:52 -0400

    added comments

--- Transmitter_Code/Transmitter_Code.ino
+++ Transmitter_Code/Transmitter_Code.ino
@@ -2,9 +2,11 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radioModule(7, 8); // CE, CSN
+// CE, CSN pins for the NRF24L01 module
+RF24 radioModule(7, 8);
 const byte address[6] = "00004";
 
+//declarations for the communication with the GUI over serial
 const byte sizeOfBuffer = 40;
 char inputFromPC[sizeOfBuffer];
 const char startMarker = '<';
@@ -12,28 +14,29 @@ const char endMarker = '>';
 byte numbOfBytesReceived = 0;
 boolean readInProgress = false;
 boolean newMessageReceived = false;
-
-char triggerChar = '7';
-
 unsigned long curMillis;
-
 unsigned long prevReplyToPCmillis = 0;
 unsigned long replyToPCinterval = 1000;
 
+//char container for open/close command input
+char triggerChar = '7';
 
 void setup() {
   Serial.begin(9600);
-
+  
+  //setup the NRF24L01 module in transmit mode
   radioModule.begin();
   radioModule.openWritingPipe(address);
   radioModule.setPALevel(RF24_PA_MIN);
   radioModule.stopListening();
   
+  //send arduino is ready message to pc
   Serial.println("<Arduino is ready>");
 }
 
 
 void loop() {
+  //get incoming commands from pc and reply with confirmation message
   curMillis = millis();
   getDataFromPC();
   replyToPC();
@@ -41,11 +44,13 @@ void loop() {
 
 
 void getDataFromPC() {
-    
+   
+  //checks if there is a message available to be read
   if(Serial.available() > 0) {
 
     char currentChar = Serial.read();
-      
+     
+    //if the char that's been read is the end marker, the message has ended and program moves on the sendCommand() function
     if (currentChar == endMarker) {
       readInProgress = false;
       newMessageReceived = true;
@@ -53,6 +58,7 @@ void getDataFromPC() {
       sendCommand();
     }
     
+    //if the char that's been read is not the end marker and the read is in progress, read the incomming command
     if(readInProgress) {
       inputFromPC[numbOfBytesReceived] = currentChar;
       numbOfBytesReceived ++;
@@ -60,7 +66,8 @@ void getDataFromPC() {
         numbOfBytesReceived = sizeOfBuffer - 1;
       }
     }
-
+    
+    //if the char that's been read is the start marker, turns readInProgress to true
     if (currentChar == startMarker) { 
       numbOfBytesReceived = 0; 
       readInProgress = true;
@@ -70,16 +77,19 @@ void getDataFromPC() {
 
  
 void sendCommand() {
+  //takes the input from pc and changes it to a form that can be sent over the NRF24L01 module
   triggerChar = inputFromPC[0];
   char text[32] = "7";
   text[0] = triggerChar;
-
+  
+  //sends the input command over the NRF23L01 module
   radioModule.write(&text, sizeof(text));
 }
 
 
 void replyToPC() {
   
+  //sends the confirmation message back to the pc
   if (newMessageReceived) {
     newMessageReceived = false;
     Serial.print("<Received Command: ");
