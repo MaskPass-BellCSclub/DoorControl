5b7ef2e075455985668805f3f22c7e6fc846846e
Anthero1 <45021288+Anthero1@users.noreply.github.com>
2021-07-25 13:35:20 -0400

    Added comments

--- Receiver_Code/Receiver_Code.ino
+++ Receiver_Code/Receiver_Code.ino
@@ -2,16 +2,22 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+//Pin that controls the NPN transistor
 int triggerPin = 3;
 char message[32] = "";
+
+//CE and CSN pins of the nrf24l01 module
 RF24 receiver(5, 6);
+//address for the radio module
 const byte address[6] = "00004";
 
 void setup() {
   Serial.begin(9600);
   
+  //sets the NPN control pin to ouput
   pinMode(triggerPin, OUTPUT);
   
+  //starts up the NRF24L01 module in receiver mode
   receiver.begin();
   receiver.openReadingPipe(0, address);
   receiver.setPALevel(RF24_PA_MIN);
@@ -19,8 +25,13 @@ void setup() {
 }
 
 void loop() {
+  //checks to see if a radio message is available on the specified address
   if (receiver.available()) {
+    
+    //reads in the message
     receiver.read(&text, sizeof(text));
+    
+    //checks the received message and if it's 0 opens the door, otherwise closes the door
     if(text[0] == '0'){
       digitalWrite(triggerPin, 255);
       Serial.println("Opening door");
